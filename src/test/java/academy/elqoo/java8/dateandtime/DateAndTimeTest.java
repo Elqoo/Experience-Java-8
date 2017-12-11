836262c8096e1d230a82a12aafbfdf8460db52f6
Stijn De Mulder <stijn.demulder@elqoo.com>
2017-12-11 21:19:04 +0100

    Small goto next month fixes

--- src/test/java/academy/elqoo/java8/dateandtime/DateAndTimeTest.java
+++ src/test/java/academy/elqoo/java8/dateandtime/DateAndTimeTest.java
@@ -27,9 +27,9 @@ public class DateAndTimeTest {
     public void shouldGotoFirstOfNextMonth(){
         LocalDate newYearsEve = DateTime8.createNewYearsEve2017();
         LocalDate firstJanuary2018 = null;
-        assertThat(newYearsEve.getYear(), is(equalTo(2010)));
-        assertThat(newYearsEve.getMonth(), is(equalTo(1)));
-        assertThat(newYearsEve.getDayOfMonth(), is(equalTo(1)));
+        assertThat(firstJanuary2018.getYear(), is(equalTo(2018)));
+        assertThat(firstJanuary2018.getMonth(), is(equalTo(Month.DECEMBER)));
+        assertThat(firstJanuary2018.getDayOfMonth(), is(equalTo(1)));
     }
 
     @Test
