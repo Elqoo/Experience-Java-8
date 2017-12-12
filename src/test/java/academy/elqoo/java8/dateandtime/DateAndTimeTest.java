9901958b8a3c86359067c3a5f427b70761c4029b
Stijn De Mulder <stijn.demulder@elqoo.com>
2017-12-12 08:42:31 +0100

    Fix shouldCreateLocalDateTimeNewYearsEve

--- src/test/java/academy/elqoo/java8/dateandtime/DateAndTimeTest.java
+++ src/test/java/academy/elqoo/java8/dateandtime/DateAndTimeTest.java
@@ -71,9 +71,9 @@ public class DateAndTimeTest {
     @Test
     public void shouldCreateLocalDateTimeNewYearsEve(){
         LocalDateTime newYearsEve = null; // create LocalDateTime for new years eve at midnight
-        assertThat(newYearsEve.getYear(), is(equalTo(2010)));
-        assertThat(newYearsEve.getMonth(), is(equalTo(1)));
-        assertThat(newYearsEve.getDayOfMonth(), is(equalTo(1)));
+        assertThat(newYearsEve.getYear(), is(equalTo(2017)));
+        assertThat(newYearsEve.getMonth(), is(equalTo(Month.DECEMBER)));
+        assertThat(newYearsEve.getDayOfMonth(), is(equalTo(31)));
         assertThat(newYearsEve.getHour(), is(equalTo(0)));
         assertThat(newYearsEve.getMinute(), is(equalTo(0)));
         assertThat(newYearsEve.getSecond(), is(equalTo(0)));
